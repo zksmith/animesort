5203320f0dd21f46037037dfc840045b867c17f0
zksmith <zahiersmith96@gmail.com>
2020-07-20 02:32:57 -0400

    moved homepage state code to home.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/home';
 import AnimePage from './pages/anime';
@@ -6,80 +6,11 @@ import SearchPage from './pages/search';
 import ListPage from './pages/list';
 
 function App() {
-  const [currentSeason, setCurrentSeason] = useState(null);
-  const [airingData, setAiringData] = useState(null);
-  const [mostPopular, setMostPopularData] = useState(null);
-
-  useEffect(() => {
-    const fetchMostPopular = async () => {
-      const response = await fetch(
-        'https://api.jikan.moe/v3/top/anime/1/bypopularity'
-      );
-
-      if (response.ok) {
-        const json = await response.json();
-
-        setMostPopularData({
-          anime: json.top.map((item) => ({
-            title: item.title,
-            image: item.image_url,
-            id: item.mal_id,
-          })),
-        });
-      }
-    };
-
-    const fetchSeasonData = async () => {
-      const response = await fetch(
-        'https://api.jikan.moe/v3/season/2020/summer'
-      );
-
-      if (response.ok) {
-        const json = await response.json();
-
-        setCurrentSeason({
-          anime: json.anime.map((item) => ({
-            title: item.title,
-            image: item.image_url,
-            id: item.mal_id,
-          })),
-        });
-      }
-    };
-
-    const fetchAiringData = async () => {
-      const response = await fetch(
-        'https://api.jikan.moe/v3/top/anime/1/airing'
-      );
-
-      if (response.ok) {
-        const json = await response.json();
-
-        setAiringData({
-          anime: json.top.map((item) => ({
-            title: item.title,
-            image: item.image_url,
-            id: item.mal_id,
-          })),
-        });
-      }
-    };
-
-    fetchSeasonData();
-    fetchAiringData();
-    fetchMostPopular();
-    return () => {};
-  }, []);
-
   return (
     <Router>
       <Switch>
         <Route path='/' exact>
-          <HomePage
-            airingData={airingData}
-            currentSeasonData={currentSeason}
-            mostPopularData={mostPopular}
-          />
+          <HomePage />
         </Route>
         <Route path='/anime/:id'>
           <AnimePage />
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header/';
 import Background from '../components/Background/';
 import CardRow from '../components/CardRow';
 
-function HomePage({ airingData, currentSeasonData, mostPopularData }) {
+function HomePage() {
+  const [currentSeasonData, setCurrentSeason] = useState(null);
+  const [airingData, setAiringData] = useState(null);
+  const [mostPopularData, setMostPopularData] = useState(null);
+
+  useEffect(() => {
+    const fetchMostPopular = async () => {
+      const response = await fetch(
+        'https://api.jikan.moe/v3/top/anime/1/bypopularity'
+      );
+
+      if (response.ok) {
+        const json = await response.json();
+
+        setMostPopularData({
+          anime: json.top.map((item) => ({
+            title: item.title,
+            image: item.image_url,
+            id: item.mal_id,
+          })),
+        });
+      }
+    };
+
+    const fetchSeasonData = async () => {
+      const response = await fetch(
+        'https://api.jikan.moe/v3/season/2020/summer'
+      );
+
+      if (response.ok) {
+        const json = await response.json();
+
+        setCurrentSeason({
+          anime: json.anime.map((item) => ({
+            title: item.title,
+            image: item.image_url,
+            id: item.mal_id,
+          })),
+        });
+      }
+    };
+
+    const fetchAiringData = async () => {
+      const response = await fetch(
+        'https://api.jikan.moe/v3/top/anime/1/airing'
+      );
+
+      if (response.ok) {
+        const json = await response.json();
+
+        setAiringData({
+          anime: json.top.map((item) => ({
+            title: item.title,
+            image: item.image_url,
+            id: item.mal_id,
+          })),
+        });
+      }
+    };
+
+    fetchSeasonData();
+    fetchAiringData();
+    fetchMostPopular();
+    return () => {};
+  }, []);
+
   return (
     <>
       <Background />
