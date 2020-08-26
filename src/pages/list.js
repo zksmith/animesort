15c1a5fd1c958f010f4d052559b3b46e964701ab
zksmith <zahiersmith96@gmail.com>
2020-08-26 10:46:44 -0400

    updated list page error handling and rendering

--- src/pages/list.js
+++ src/pages/list.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import CardGrid from '../components/CardGrid';
+import LoadingIndicator from '../components/LoadingIndicator';
 
 function ListPage() {
   const { username } = useParams();
   const [listData, setListData] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const getListData = async () => {
@@ -14,22 +16,41 @@ function ListPage() {
         );
 
         const json = await response.json();
-        console.log(json);
-        setListData(json);
+        if (response.ok) {
+          console.log(json);
+          setListData(json);
+        } else {
+          throw json;
+        }
       } catch (err) {
         console.log(err);
+        setError(true);
       }
     };
 
     getListData();
   }, [username]);
 
-  return (
-    <div className='container'>
-      <h2>{listData?.username}'s List</h2>
-      {listData?.list && <CardGrid array={listData.list} />}
-    </div>
-  );
+  if (error) {
+    return (
+      <div className='container'>
+        <h2>Unable to load data for {username}</h2>
+      </div>
+    );
+  } else {
+    return (
+      <div className='container'>
+        {listData ? (
+          <>
+            <h2>{listData?.username}'s List</h2>
+            {listData?.list && <CardGrid array={listData.list} />}
+          </>
+        ) : (
+          <LoadingIndicator />
+        )}
+      </div>
+    );
+  }
 }
 
 export default ListPage;
